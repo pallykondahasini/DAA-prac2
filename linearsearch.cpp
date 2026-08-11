3574b87c3de0fc0328b7bf73c799d41d142ee37d
pallykondahasini <pallykondahasini.128390@marwadiuniversity.ac.in>
2026-08-11 10:09:55 +0530

    Implement linear search in linearsearch.cpp

---
+++ linearsearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int key = 30;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout << "Element found at index " << i;
+            return 0;
+        }
+    }
+
+    cout << "Element not found";
+
+    return 0;
+}
